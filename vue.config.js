16b5287bf4004fc96b51b6381644b6fb13bbd514
xjh <15200180653@163.com>
2023-04-23 10:16:14 +0800

    调整默认图标

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+console.info(process.env.NODE_ENV,"====");
 console.info(process.env.VUE_APP_ENV_PATH,"====");
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -12,9 +13,37 @@ module.exports = defineConfig({
     //   navigateFallback: '/',
     //   navigateFallbackBlacklist: [/\/api\//]
     // }
+    iconPaths: {
+
+      favicon32: 'favicon.ico',
+
+      favicon16: 'favicon.ico',
+
+      appleTouchIcon: 'favicon.ico',
+
+      maskIcon: 'favicon.ico',
+
+      msTileImage: 'favicon.ico',
+
+    }
   },
-  devServer: {
-    // publicPath: './',
+  devServer: process.env.NODE_ENV === 'development' ?{
+    host: "localhost",
+    port: `8089`,
+    open: true,
+    // 代理链接配置
+    proxy: {
+      "/api": {
+        target: process.env.VUE_APP_ENV_PATH,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        },
+        secure: true,
+      }
+    }
+  } :{
+    publicPath: './',
     host: "localhost",
     port: `8089`,
     open: true,
